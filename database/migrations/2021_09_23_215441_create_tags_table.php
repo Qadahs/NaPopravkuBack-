0b64f5eb3944846b855e390bb3a51b8f751634cb
Qadahs <mixeevnickita@yandex.ru>
2021-09-24 02:17:11 +0300

    Fixed migrations

--- database/migrations/2021_09_23_215441_create_tags_table.php
+++ database/migrations/2021_09_23_215441_create_tags_table.php
@@ -15,7 +15,7 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id()->index();
-            $table->text('Title');
+            $table->text('title');
         });
     }
 
