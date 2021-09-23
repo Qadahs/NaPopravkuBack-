fa9464052e12cb256f313e5d5e496afc11725b7d
Qadahs <mixeevnickita@yandex.ru>
2021-09-24 02:06:19 +0300

    Start of the new project.

---
+++ database/migrations/2021_09_23_215519_create_user_article_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_article', function (Blueprint $table) {
+            $table->id()->index();
+            $table->unsignedBigInteger('users_id')->index();
+            $table->unsignedBigInteger('articles_id')->index();
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('articles_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_article');
+    }
+}
---
+++ database/migrations/2021_09_23_215530_create_article_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->id()->index();
+            $table->unsignedBigInteger('articles_id')->index();
+            $table->unsignedBigInteger('tags_id')->index();
+            $table->foreign('articles_id')->references('id')->on('articles');
+            $table->foreign('tags_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_tag');
+    }
+}
